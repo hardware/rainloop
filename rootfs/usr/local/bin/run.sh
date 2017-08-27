aacde245330c8edb1a45838fffedc5190780f566
Hardware <contact@meshup.net>
2017-08-28 00:37:06 +0200

    fix(): potential security gap #9

--- rootfs/usr/local/bin/run.sh
+++ rootfs/usr/local/bin/run.sh
@@ -4,7 +4,7 @@
 sed -i "s/<UPLOAD_MAX_SIZE>/$UPLOAD_MAX_SIZE/g" /etc/php7/php-fpm.conf /etc/nginx/nginx.conf
 
 # Fix permissions
-chown -R $UID:$GID /rainloop /services /var/log /var/lib/nginx
+chown -R $UID:$GID /rainloop/data /services /var/log /var/lib/nginx
 
 # RUN !
 exec su-exec $UID:$GID /bin/s6-svscan /services
